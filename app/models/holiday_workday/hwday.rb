f304f2211b3b483607213eb3d78dbfd8a093807a
iiithinking <wuyingchao1@gmail.com>
2021-04-12 17:31:17 +0800

    add worked_weeks method

--- app/models/holiday_workday/hwday.rb
+++ app/models/holiday_workday/hwday.rb
@@ -12,6 +12,10 @@ module HolidayWorkday
       holiday.where(date: range).pluck(:date)
     end
 
+    def self.worked_days(range)
+      range.reject{|x| x.sunday? || x.saturday?} - holidays(range) + workdays(range)
+    end
+
     def self.weeks(range)
       weeks = []
       week_start = range.first
@@ -26,6 +30,29 @@ module HolidayWorkday
         weeks << week
         break if week_end == range.last
 
+        week_start = week_end.next_day
+        week_end = week_start.end_of_week
+      end
+      weeks
+    end
+
+    def self.workday_weeks(range)
+      weeks = []
+      week_start = range.first
+      workdays = workdays(range)
+      holidays = holidays(range)
+      return weeks if week_start > range.last
+
+      week_end = week_start.at_end_of_week
+      loop do
+        break if week_start > range.last
+
+        week_end = range.last if week_end > range.last
+        week = (week_start..week_end).reject{|x| x.sunday? || x.saturday?}
+
+        week = week - holidays + workdays
+        weeks << week if week.present?
+        break if week_end == range.last
         week_start = week_end.next_day
         week_end = week_start.end_of_week
       end
